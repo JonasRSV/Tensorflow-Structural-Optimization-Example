09da55677f1556175e293dd00d095a1bfb204ad2
Jonas Valfridsson <jonas@valfridsson.net>
2020-04-20 12:29:09 +0200

    update penalty

--- struct_opt/penalty.py
+++ struct_opt/penalty.py
@@ -78,14 +78,9 @@ class EntropyStructurePenalty:
     def get_penalty_function(self):
         def penalty_function(sgm_design: tf.Tensor):
             self.epoch += 1
+            raise NotImplementedError("Not yet implemented")
 
             if self.epochs - self.epoch < self.penalty_epochs:
-                if self.entropy_norm is None:
-                    self.entropy_norm = -tf.cast(tf.size(sgm_design), dtype=tf.float32) * 0.5 * tf.math.log(0.5)
-
-                entropy = -tf.reduce_sum(sgm_design * tf.math.log(sgm_design)
-                                         + (1 - sgm_design) * tf.math.log(1 - sgm_design)) / self.entropy_norm
-
                 return self.penalty_size * (self.epoch / self.epochs) * entropy
             else:
                 return 0.0
